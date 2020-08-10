c313e2e285f5c5addd9b8541be268740d935c17e
riki0125 <rgenaizir@mobydigital.com>
2020-08-10 10:28:26 -0300

    descomentado de usuarioDAO

--- src/main/java/com/mobydigitalrrhh/models/dao/IUsuarioDAO.java
+++ src/main/java/com/mobydigitalrrhh/models/dao/IUsuarioDAO.java
@@ -3,8 +3,7 @@ package com.mobydigitalrrhh.models.dao;
 import org.springframework.data.repository.CrudRepository;
 import com.mobydigitalrrhh.models.entities.Usuario;
 
-/*
+
 public interface IUsuarioDAO extends CrudRepository<Usuario, Integer>{
 
 }
-*/
