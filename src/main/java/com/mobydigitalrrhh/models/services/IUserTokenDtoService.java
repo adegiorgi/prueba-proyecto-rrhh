97eef95629f343ee99faefef73448942d7bec010
riki0125 <rgenaizir@mobydigital.com>
2020-08-19 13:23:08 -0300

    creacion de views y aparente camino a seguir de relaciones

--- src/main/java/com/mobydigitalrrhh/models/services/IUserTokenDtoService.java
+++
@@ -1,8 +0,0 @@
-package com.mobydigitalrrhh.models.services;
-
-import com.mobydigitalrrhh.models.views.UserToken;
-
-public interface IUserTokenDtoService {
-
-	public UserToken traerUsuarioyToken(String email);
-}
